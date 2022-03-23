d0030881d51bed379c5672fe899c10bd0bb419d1
Marc Durham <marcdurham@gmail.com>
2022-03-23 14:14:39 -0700

    Reverse count logic

--- MailerRestApi/Program.cs
+++ MailerRestApi/Program.cs
@@ -80,7 +80,7 @@ app.MapGet("/health", () =>
             }
         }
         
-        if (missingFiles.Count == scheduleFiles.Length)
+        if (missingFiles.Count != scheduleFiles.Length)
         {
             app.Logger.LogInformation($"Health: Green");
             return Results.Ok("Green");
