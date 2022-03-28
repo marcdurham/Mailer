eff80f209598726e162e6e9a3184b6bdafe71400
Marc Durham <marcdurham@gmail.com>
2022-03-27 18:35:58 -0700

    Add razor page for meetings/* raw HTML

---
+++ MailerRestApi/Pages/Meetings.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNet6.Pages;
+
+public class MeetingsModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+    private readonly IConfiguration _configuration;
+
+    public MeetingsModel(ILogger<IndexModel> logger, IConfiguration configuration)
+    {
+        _logger = logger;
+        _configuration = configuration;
+    }
+
+    public string RawHtml { get; set; } = String.Empty;
+
+    public IActionResult OnGet(string name)
+    {
+        string scheduleFolder = _configuration.GetValue<string>("Schedules:StaticScheduleRootFolder")!;
+        string schedFilePath = $"{Path.Combine(scheduleFolder, name)}.html";
+        if (!System.IO.File.Exists(schedFilePath))
+        {
+            return NotFound();
+        }
+
+        RawHtml = System.IO.File.ReadAllText(schedFilePath);
+
+        return Page();
+    }
+}
