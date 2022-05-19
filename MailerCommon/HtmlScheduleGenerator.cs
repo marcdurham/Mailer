444efbf1febf06348d011d13826bb8ade93f428c
Marc Durham <marcdurham@gmail.com>
2022-05-18 18:42:23 -0700

    Add error catching

--- MailerCommon/HtmlScheduleGenerator.cs
+++ MailerCommon/HtmlScheduleGenerator.cs
@@ -13,7 +13,7 @@ public class HtmlScheduleGenerator
             html = html.Replace($"@{{DayOfWeek{wk}}}", meetings[wk].Date.ToString("dddd"));
         }
 
-        Regex regex = new Regex(@"@{.+}");
+        Regex regex = new(@"@{.+}");
 
         MatchCollection? matches = regex.Matches(html);
 
@@ -21,58 +21,97 @@ public class HtmlScheduleGenerator
         {
             string value = match.Value;
             string replacement = string.Empty;
-            string friendKey = "none";
             Match m = Regex.Match(value, @"@{([^:]+)(:(\d+))?(:([^:0-9]+))?}");
             if (m.Success)
             {
                 string key = m.Groups[1].Value;
                 string index = m.Groups[3].Value;
                 string property = m.Groups[5].Value;
-
-                int.TryParse(index, out int indexValue);
-
-                if (meetings[indexValue].Assignments.ContainsKey(key))
-                {
-                    friendKey = meetings[indexValue].Assignments[key].Friend.Key;
-                    if (string.Equals(property, "N") || string.Equals(property, "Name"))
-                    {
-                        replacement = $"{meetings[indexValue].Assignments[key].Name}";
-                    }
-                    else if (string.Equals(property, "E"))
-                    {
-                        replacement = $"{meetings[indexValue].Assignments[key].Friend.EnglishName}";
-                    }
-                    else if (string.Equals(property, "CHS"))
-                    {
-                        replacement = $"{meetings[indexValue].Assignments[key].Friend.SimplifiedChineseName}";
-                    }
-                    else if (string.Equals(property, "E/PY/CHS"))
-                    {
-                        replacement = $"{meetings[indexValue].Assignments[key].Friend.PinYinName}<br/>{ meetings[indexValue].Assignments[key].Friend.SimplifiedChineseName}<br/>{ meetings[indexValue].Assignments[key].Friend.Name}";
-                    }
-                    else if (string.Equals(property, "CHS/E"))
-                    {
-                        replacement = $"{meetings[indexValue].Assignments[key].Friend.SimplifiedChineseName} {meetings[indexValue].Assignments[key].Friend.Name}";
-                    }
-
-                    else if (string.Equals(property, "NoService"))
-                    {
-                        replacement = meetings[indexValue].Assignments[key].Friend.Name;
-                        if(replacement.Contains("/"))
-                            replacement = meetings[indexValue].Assignments[key].Friend.Name.Split("/").Last();
-                    }
-                    else
-                    {
-                        replacement = $"{meetings[indexValue].Assignments[key].Friend.PinYinName}<br/>{ meetings[indexValue].Assignments[key].Friend.SimplifiedChineseName}<br/>{ meetings[indexValue].Assignments[key].Friend.Name}";
-                    }
-                }
+                
+                replacement = ParseAndReplace(meetings, value, key, index, property);
             }
 
             string escaped = value.Replace("(", "\\(").Replace(")", "\\)");
-            html = Regex.Replace(html, $"<td(\\s+class=\")?([a-zA-Z0-9-]+)?(\")?>({escaped})</td>", $"<td$1$2$3 data-friend-key='{friendKey}'>{replacement}</td>");
+            string pattern = $"<td(\\s+class=\")?([a-zA-Z0-9-]+)?(\")?>({escaped})</td>";
+
+            html = Regex.Replace(html, pattern, replacement);
         }
 
         return html;
+    }
+
+    private static string ParseAndReplace(List<Meeting> meetings, string value, string key, string index, string property)
+    {
+        try
+        {
+            string replacement = string.Empty;
+            string friendKey = string.Empty;
+            int.TryParse(index, out int indexValue);
+
+            if (meetings[indexValue].Assignments.ContainsKey(key))
+            {
+                friendKey = meetings[indexValue].Assignments[key].Friend.Key;
+
+                replacement = GetReplacementFor(
+                    meetings[indexValue].Assignments[key],
+                    property);
+            }
+
+            return $"<td$1$2$3 data-friend-key='{friendKey}'>{replacement}</td>";
+        }
+        catch (Exception ex)
+        {
+            throw new Exception($"Error getting replacement for data-friend-key: at value: {value} key: {key} index: {index} property: {property}", ex);
+        }
+    }
+
+    private static string GetReplacementFor(Assignment assignment, string property)
+    {
+        string replacement;
+
+        try
+        {
+            if (string.Equals(property, "N") || string.Equals(property, "Name"))
+            {
+                replacement = $"{assignment.Name}";
+            }
+            else if (string.Equals(property, "E"))
+            {
+                replacement = $"{assignment.Friend.EnglishName}";
+            }
+            else if (string.Equals(property, "CHS"))
+            {
+                replacement = $"{assignment.Friend.SimplifiedChineseName}";
+            }
+            else if (string.Equals(property, "E/PY/CHS"))
+            {
+                replacement = $"{assignment.Friend.PinYinName}<br/>{ assignment.Friend.SimplifiedChineseName}<br/>{ assignment.Friend.Name}";
+            }
+            else if (string.Equals(property, "CHS/E"))
+            {
+                replacement = $"{assignment.Friend.SimplifiedChineseName} {assignment.Friend.Name}";
+            }
+            else if (string.Equals(property, "NoService"))
+            {
+                replacement = assignment.Friend.Name;
+                if (replacement.Contains("/"))
+                {
+                    replacement = assignment.Friend.Name.Split("/").Last();
+                }
+            }
+            else
+            {
+                replacement = $"{assignment.Friend.PinYinName}<br/>{ assignment.Friend.SimplifiedChineseName}<br/>{ assignment.Friend.Name}";
+            }
+        }
+        catch (Exception ex)
+        {
+            throw new Exception(
+                message: $"Error finding replacement for property: {property} with assignment: {assignment.Name} with friend name: {(assignment.Friend?.Name ?? "NULL")}", 
+                innerException: ex);
+        }
+
+        return replacement;
     }
 
     public static (string html, List<Assignment> friendAssignments) InjectUpcomingAssignments(
