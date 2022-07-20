9b53ee1c3f1302b51b9e73ee693a885c05cf7124
Marc Durham <marcdurham@gmail.com>
2022-07-20 14:46:43 -0700

    Extract a Rotate sheet method

--- MailerCommon/ScheduleLoader.cs
+++ MailerCommon/ScheduleLoader.cs
@@ -5,12 +5,11 @@ namespace MailerCommon
     public class ScheduleLoader
     {
         public static List<Meeting> GetSchedule(
-            IList<IList<object>> valuesIn, 
+            IList<IList<object>> sheet, 
             Dictionary<string, Friend> friendMap, 
             ScheduleInputs scheduleInputs)
         {
             const int HeaderRowIndex = 0;
-            int[] daysOfWeek = new int[] { (int)scheduleInputs.MeetingDayOfWeek };
             string name = scheduleInputs.MeetingName;
             string title = scheduleInputs.MeetingTitle;
             TimeOnly? meetingStartTime = scheduleInputs.MeetingStartTime.HasValue
@@ -19,35 +18,10 @@ namespace MailerCommon
             int mondayColumnIndex = 0;
             int meetingDateColumnIndex = scheduleInputs.MeetingDateColumnIndex ?? 0;
 
-            // Rotate Sheet
-            IList<IList<object>> values = valuesIn;
-            if(scheduleInputs.ColumnIsDay)
-            {
-                values = new List<IList<object>>();
-                int rowCount = valuesIn.Count;
-                int colCount = valuesIn[HeaderRowIndex].Count;
-                // Add columns as rows
-                for (int c = HeaderRowIndex; c < colCount; c++)
-                {
-                    values.Add(new List<object>());
-                }
-
-                // Add each column c to each row r               
-                for (int c = HeaderRowIndex; c < colCount; c++)
-                {
-                    for(int r = 0; r < rowCount; r++)
-                    {
-                        if (valuesIn[r].Count > c)
-                        {
-                            values[c].Add(valuesIn[r][c]);
-                        }
-                        else
-                        {
-                            values[c].Add(null);
-                        }
-                    }
-                }
-            }
+            // Rotate sheet if needed
+            IList<IList<object>> values = scheduleInputs.ColumnIsDay 
+                ? Rotate(sheet, HeaderRowIndex)
+                : sheet;
 
             if (values == null || values.Count == 0)
                 return new List<Meeting>();
@@ -101,6 +75,37 @@ namespace MailerCommon
             }
 
             return meetings;
+        }
+
+        private static IList<IList<object>> Rotate(IList<IList<object>> sheet, int headerRowIndex)
+        {
+            IList<IList<object>> values = sheet;
+            values = new List<IList<object>>();
+            int rowCount = sheet.Count;
+            int colCount = sheet[headerRowIndex].Count;
+            // Add columns as rows
+            for (int c = headerRowIndex; c < colCount; c++)
+            {
+                values.Add(new List<object>());
+            }
+
+            // Add each column c to each row r               
+            for (int c = headerRowIndex; c < colCount; c++)
+            {
+                for (int r = 0; r < rowCount; r++)
+                {
+                    if (sheet[r].Count > c)
+                    {
+                        values[c].Add(sheet[r][c]);
+                    }
+                    else
+                    {
+                        values[c].Add(null!);
+                    }
+                }
+            }
+
+            return values;
         }
 
         static Assignment MapAssignment(
