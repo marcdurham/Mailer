e6663688fb1bfaa231e5d8bc1311dfd7836d572b
Marc Durham <marcdurham@gmail.com>
2022-07-21 00:28:31 -0700

    Add more logic

--- PdfReader/Pdf.cs
+++ PdfReader/Pdf.cs
@@ -13,6 +13,7 @@ namespace PdfReader
             var builder = new StringBuilder(10000);
             using (PdfDocument document = PdfDocument.Open(filePath))
             {
+                List<Assignment> assignments = new();
                 for (var i = 0; i < document.NumberOfPages; i++)
                 {
                     // This starts at 1 rather than 0.
@@ -77,29 +78,44 @@ namespace PdfReader
                     builder.AppendLine("LINES:");
                     //for(int l = 0; l < lines.Count; l++)
                     string weekHeader = string.Empty;
+                    string sectionHeader = string.Empty;
                     foreach(Line lin in lineObjects)
                     {
                         builder.AppendLine($"{lineObjects.IndexOf(lin)}:: {lin.AllText()}");
                         if(IsWeekHeader(lin) && lin.ContainsBoxText("|"))
                         {
                             weekHeader = lin.AllText().Split('|')[0];
+                            sectionHeader = string.Empty;
                         }
+
+                        if(IsSectionHeader(lin))
+                        {
+                            sectionHeader = lin.Boxes[0].Text;
+                        }
+
                         foreach(var box in lin.Boxes)
                         {
                             builder.AppendLine(box.Text);
                         }
 
-                        var assignments = Process(lin, weekHeader);
+                        assignments.AddRange(Process(lin, sectionHeader, weekHeader));
 
-                        foreach(Assignment assignment in assignments)
-                        {
-                            builder.AppendLine($"ASSIGNMENT: WK: {assignment.WeekHeader} ST: {assignment.Start} MIN: {assignment.Minutes} AN: {assignment.AssignmentName}");
-                        }
+                        //foreach(Assignment assignment in assignments)
+                        //{
+                        //    builder.AppendLine($"ASSIGNMENT: WK: {assignment.WeekHeader} SEC: {assignment.Section} ST: {assignment.Start} MIN: {assignment.Minutes} AN: {assignment.AssignmentName} To: {assignment.AssigneeName}");
+                        //}
                         builder.AppendLine();
                     }
 
                 }
+                builder.AppendLine("ALL ASSIGNMENTS:");
+
+                foreach (Assignment assignment in assignments)
+                {
+                    builder.AppendLine($"WK: {assignment.WeekHeader} SEC: {assignment.Section} ST: {assignment.Start} MIN: {assignment.Minutes} AN: {assignment.AssignmentName} To: {assignment.AssigneeName}");
+                }
             }
+
             return builder.ToString();
         }
 
@@ -112,41 +128,106 @@ namespace PdfReader
                 && line.ContainsBoxText("日");
         }
 
-        List<Assignment> Process(Line line, string weekHeader)
+        bool IsSectionHeader(Line line)
         {
-            if(line.Boxes.Count > 3 && Regex.IsMatch(line.Boxes[0].Text, @"^\d+:\d\d$"))
+            return line.Boxes.Count > 0 && (
+                   line.Boxes[0].Text == "上帝話語的寶藏"
+                || line.Boxes[0].Text == "用心準備傳道工作"
+                || line.Boxes[0].Text == "基督徒的生活");
+        }
+
+        List<Assignment> Process(Line line, string sectionHeader, string weekHeader)
+        {
+            List<Assignment> assignments = new();
+            var startlessPattern = new Regex(@"(主席|第二班導師)：(.*)");
+            var startlessMatch = startlessPattern.Match(line.AllText());
+            if (startlessMatch.Success)
+            {
+                Assignment assignment = new()
+                {
+                    WeekHeader = weekHeader,
+                    Section = sectionHeader,
+                    AssignmentName = startlessMatch.Groups[1].Value,
+                    AssigneeName = startlessMatch.Groups[2].Value,
+                };
+
+                assignments.Add(assignment);
+            }
+
+            var prayerPattern = new Regex(@"(\d+:\d\d)•(.*)(禱告)：(.*)");
+            var prayerMatch = prayerPattern.Match(line.AllText());
+            if (prayerMatch.Success)
+            {
+                Assignment assignment = new()
+                {
+                    WeekHeader = weekHeader,
+                    Section = sectionHeader,
+                    Start = TimeOnly.Parse(prayerMatch.Groups[1].Value),
+                    AssignmentName = prayerMatch.Groups[3].Value,
+                    AssigneeName = prayerMatch.Groups[4].Value,
+                };
+
+                assignments.Add(assignment);
+            }
+
+            if (line.Boxes.Count > 3 && Regex.IsMatch(line.Boxes[0].Text, @"^\d+:\d\d$"))
             {
                 string assignmentName = line.Boxes[2].Text;
                 int minutes = 0;
-                //if(line.Boxes[3].Text == "分鐘）" && Regex.IsMatch(line.Boxes[2].Text, @"(.*)（\d+"))
-                //{
-                //    assignmentName = Regex.Replace(line.Boxes[2].Text, @"（\d+$", "");
-                //    string minutesText = line.Boxes[2].Text.Split('（').Last();
-                //    minutes = int.Parse(minutesText);
-                //}
-
-                var minutesPattern = new Regex(@"\d+:\d\d•(.*)[（(](\d+)分鐘[）)]");
+                string assigneeName = string.Empty;
+                var minutesPattern = new Regex(@"\d+:\d\d•(.*)[（(](\d+)分鐘[）)](.*)");
                 var match = minutesPattern.Match(line.AllText());
                 if(match.Success)
                 {
+                    assignmentName = match.Groups[1].Value;
                     string minutePart = match.Groups[2].Value;
                     minutes = int.Parse(minutePart);
-                    assignmentName = match.Groups[1].Value;
+                    assigneeName = match.Groups[3].Value;
                 }
 
-                var assignment = new Assignment
+                Assignment assignment = new()
                 {
                     WeekHeader = weekHeader,
+                    Section = sectionHeader,
                     Start = TimeOnly.Parse(line.Boxes[0].Text),
                     AssignmentName = assignmentName,
-                    //AssigneeName = line.Boxes[2].Text,
+                    AssigneeName = assigneeName,
                     Minutes = minutes
                 };
 
-                return new List<Assignment> { assignment };
+                if(assignment.AssignmentName == "唱詩第")
+                {
+                    assignment.AssignmentName = $"唱詩第{line.Boxes[3].Text}";
+                }
+                else if(assignment.AssignmentName == "會眾研經班")
+                {
+                    assignment.AssigneeName = assignment.AssigneeName.Replace("主持人/朗讀員：", "");
+                    if(assignment.AssigneeName.Contains("/"))
+                    {
+                        Assignment secondAssignment = new()
+                        {
+                            WeekHeader = assignment.WeekHeader,
+                            Section = sectionHeader,
+                            Start = assignment.Start,
+                            AssignmentName = $"{assignment.AssignmentName} 朗讀員",
+                            AssigneeName = assignment.AssigneeName.Split("/", StringSplitOptions.TrimEntries)[1],
+                            Minutes = assignment.Minutes
+                        };
+
+                        assignment.AssigneeName = assignment.AssigneeName.Split("/", StringSplitOptions.TrimEntries)[0];
+                        assignment.AssignmentName = $"{assignment.AssignmentName} 主持人";
+                        assignments.Add(secondAssignment);
+                    }
+                }
+                else if(assignment.AssigneeName.StartsWith("學生/助手："))
+                {
+                    assignment.AssigneeName = assignment.AssigneeName.Replace("學生/助手：", "");
+                }
+
+                assignments.Add(assignment);
             }
 
-            return new List<Assignment>();
+            return assignments;
         }
 
         public string NextWordText(int w, List<Word> words)
